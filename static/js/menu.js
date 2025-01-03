f34cac38ad2bfdee69e92b7e1d604c245cc1f709
lanius <mohammedno1@proton.me>
2025-01-03 19:21:50 +0200

    fix conflict

--- static/js/menu.js
+++ static/js/menu.js
@@ -1,8 +1,19 @@
-let menu=document.querySelector(".menu");
-let menuImg=document.querySelector(".menu img");
-let links=document.querySelector(".links");
-menu.addEventListener("click",()=>{
-   links.classList.toggle('expand');
-   menuImg.classList.toggle('active');
+let menu = document.querySelector(".menu");
+let menuImg = document.querySelector(".menu img");
+let links = document.querySelector(".links");
 
-})
+
+let isExpanded = localStorage.getItem("menuExpanded") === "true";
+if (isExpanded) {
+   links.classList.add('expand');
+   menuImg.classList.add('active');
+}
+
+
+menu.addEventListener("click", () => {
+   links.classList.toggle('expand');
+   let currentState = menuImg.classList.toggle('active');
+
+
+   localStorage.setItem("menuExpanded", links.classList.contains('expand'));
+});
