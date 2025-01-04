a80fb52c63dccc64eb5a1124b2b6120b68613c9d
lanius <mohammedno1@proton.me>
2025-01-04 17:00:20 +0200

    first deploy

--- public/js/menu.js
+++
@@ -1,8 +0,0 @@
-let menu = document.querySelector(".menu");
-let menuImg = document.querySelector(".menu img");
-let links = document.querySelector(".links");
-menu.addEventListener("click", () => {
-   links.classList.toggle('expand');
-   menuImg.classList.toggle('active');
-
-})
